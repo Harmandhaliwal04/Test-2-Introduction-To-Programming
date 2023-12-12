9a86d26cf9e7148aa9aa6cc5521781be1446cada
jotdhessi <harmanjot28082004@gmail.com>
2023-12-12 14:52:11 -0500

    version4

--- Question1.py
+++ Question1.py
@@ -5,9 +5,10 @@ i=1
 while x!=6:
    
     if x==1:
-         a=input(" NAME ")
-         b=float(input(" PHONE NUMBER "))
+         a=input("Enter Name ")
+         b=int(input(" Enter your 10-digit phone number: "))
          phoneDirectory.update({a:b})
+
         
   
         
@@ -15,28 +16,30 @@ while x!=6:
 
         c=(input("Which record do you want to search "))
         if c in phoneDirectory:
-            print(c,"-",phoneDirectory[c])
+            print(c,":",phoneDirectory[c])
             print(phoneDirectory)
         else:
             print("No Record here ")         
 
     elif x==3:
-
-        phoneDirectory.update({a:b})
-        print(phoneDirectory)
+         a=input("Enter Name ")
+         b=int(input(" Enter your 10-digit phone number: "))
+         phoneDirectory.update({a:b})
+         print(phoneDirectory)
     
     elif x==4:
-         
-
+         phoneDirectory.sort()
+         print(phoneDirectory)
 
     elif x==5:
-   
-         d=(input("What record do you want to delete "))
-         if d in phoneDirectory:
+          
+
+     d=(input("What record do you want to delete "))
+     if d in phoneDirectory:
              del phoneDirectory[d]
              
              print(phoneDirectory)
-         else :
+     else :
              print("No Record here")   
 
     print("WELCOME TO THE GRANN'S PHONE DIRECTORY \n1.Add a record \n2.Search a record\n3.Update a record\n4.Sort the record alphabetically.\n5.Delete a record\n6.Quit ")
