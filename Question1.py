f0852eebc079a416af003f1ae29110d420f1dd60
jotdhessi <harmanjot28082004@gmail.com>
2023-12-12 14:07:55 -0500

    version3

--- Question1.py
+++ Question1.py
@@ -1,41 +1,63 @@
 phoneDirectory={}
-print("WELCOME TO THE GRANN'S PHONE DIRECTORY \n1.Add a record \n2.Search a record\n3.Update a record \n4.Sort the record alphabetically \n5.Delete a record\n6.Quit ")
-x = int(input("Enter Value: "))
+print("WELCOME TO THE GRANN'S PHONE DIRECTORY \n1.Add a record\n2.Search a record\n3.Update a record\n4.Sort the record alphabetically.\n5.Delete a record\n6.Quit ")
+x = int(input("Enter Your Choice: "))
 i=1
 while x!=6:
    
-   if x==1:
-       if x==1:
-         a=input("What is the name! ")
-         b=int(input(" What is the phone number "))
+    if x==1:
+         a=input(" NAME ")
+         b=float(input(" PHONE NUMBER "))
          phoneDirectory.update({a:b})
-       else:
-        print()
+        
+  
+        
+    elif x==2:
 
-
-   elif x==2:
-     c=(input(" Search a record "))
-     if c in phoneDirectory:
+        c=(input("Which record do you want to search "))
+        if c in phoneDirectory:
             print(c,"-",phoneDirectory[c])
             print(phoneDirectory)
-     else:
-            print("Not Found") 
+        else:
+            print("No Record here ")         
+
+    elif x==3:
+
+        phoneDirectory.update({a:b})
+        print(phoneDirectory)
+    
+    elif x==4:
+         
 
 
-
-   elif x==3:
-     d=(input("Whats to update"))
-     if len(phoneDirectory)>=0:
-                     print("Nothing to Update")
-     else:
-        phoneDirectory.update({a:b})   
-
-
-
-   elif x==4:
-     
-
-      
-    print("WELCOME TO THE GRANN'S PHONE DIRECTORY \n1.Add a record \n2.Search a record\n3.Update a record \n4.Sort the record alphabetically \n5.Delete a record\n6.Quit ")
-    x = int(input("Enter Value: ")) 
+    elif x==5:
    
+         d=(input("What record do you want to delete "))
+         if d in phoneDirectory:
+             del phoneDirectory[d]
+             
+             print(phoneDirectory)
+         else :
+             print("No Record here")   
+
+    print("WELCOME TO THE GRANN'S PHONE DIRECTORY \n1.Add a record \n2.Search a record\n3.Update a record\n4.Sort the record alphabetically.\n5.Delete a record\n6.Quit ")
+    x = int(input("Enter Your Choice: "))
+   
+
+
+
+
+   
+
+
+
+
+    
+
+       
+
+
+
+        
+
+
+
